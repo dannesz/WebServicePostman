4b0a6e2f5faa0255209a2a5a0845776baa9cb069
Daniel Esparza <daniel_esparza94@outlook.coma>
2021-02-21 20:39:39 -0600

    WebService consumo con Postman
    php
    mysql
    CRUD

---
+++ config/conexion.php
@@ -0,0 +1,21 @@
+<?php
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try {
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=practica_webservice","root","root");
+                return $conectar;
+            } catch (Exception $e) {
+                print "Error BD: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+
+        public function set_names() {
+            return $this->dbh->query("SET NAMES 'UTF8' ");
+        }
+    }
+?>
+
+
